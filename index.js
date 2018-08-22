4c71be351f880e1d98283d20464b4171c8ebc359
Pr0grammar <npatel3030@gmail.com>
2018-08-22 08:45:55 -0400

    added redux

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 import {name as appName} from './app.json';
 import AppSwitchNavigator from './src/navigators/AppSwitchNavigator';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
 
 
 const App = () => {
-    return <AppSwitchNavigator />
+    return (
+        <Provider store={createStore(reducers)}>
+            <AppSwitchNavigator />
+        </Provider>
+    );
 }
 
 AppRegistry.registerComponent(appName, () => App);
